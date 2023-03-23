c19205312323c4283d72cd5dcd24fca60f350511
yo_boi <abdelrahmanbbb@gmail.com>
2023-03-24 01:30:03 +0300

    started coding

--- 120200128_cmpe312_hw01.c
+++ 120200128_cmpe312_hw01.c
@@ -1,11 +1,19 @@
 #include <stdio.h>
+#include <stdlib.h>
 
+// this line defines how many rows the board will have
+#define boardSize 4
+
+// this is the number of matches in the largest row
+#define maxIs  ((2 * boardSize) + 1)
+
+struct board{
+    int array[boardSize];
+};
 
 struct player{
     int number;
 };
-
-
 
 int main(){
     puts("hello");
