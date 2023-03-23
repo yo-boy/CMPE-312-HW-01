1a1eb145422cd0e11abfbac0e36e0cd0afe294df
yo_boi <abdelrahmanbbb@gmail.com>
2023-03-24 02:10:37 +0300

    added the initial outline of functions
    Signed-off-by: yo_boi <abdelrahmanbbb@gmail.com>

--- 120200128_cmpe312_hw01.c
+++ 120200128_cmpe312_hw01.c
@@ -7,15 +7,35 @@
 // this is the number of matches in the largest row
 #define maxIs  ((2 * boardSize) + 1)
 
+
 struct board{
-    int array[boardSize];
+    int arr[boardSize];
 };
 
 struct player{
-    int number;
+    int isTurn[0];
+};
+
+void showBoard(struct board *, struct player *);
+
+void startGame();
+
+void menu();
+
+int goodMove(struct board, int, int);
+
+void playBot();
+
+void generateBoard(struct board* myBoard){
+    for (int i = 0; i < boardSize; i++){
+        // put the number of matchsticks depending on which row it is
+        myBoard->arr[i] = (2 * i) + 1;
+    }
 };
 
 int main(){
     puts("hello");
     return 0;
-}
+};
+
+
