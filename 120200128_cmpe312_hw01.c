e9763a5ebbd7f4c7a357e90c1fd6be9dc1b9cf37
yo_boi <abdelrahmanbbb@gmail.com>
2023-03-24 02:25:31 +0300

    added more functions and started real development
    Signed-off-by: yo_boi <abdelrahmanbbb@gmail.com>

--- 120200128_cmpe312_hw01.c
+++ 120200128_cmpe312_hw01.c
@@ -7,24 +7,37 @@
 // this is the number of matches in the largest row
 #define maxIs  ((2 * boardSize) + 1)
 
-
+// the state of the board will be kept in an int array
 struct board{
     int arr[boardSize];
 };
 
-struct player{
-    int isTurn[0];
-};
+// this will keep track of turns,
+// I chose to use just an int because I don't need anything more and this makes it simpler
+int player = 0;
 
-void showBoard(struct board *, struct player *);
+// this function prints a board nicely
+void showBoard(struct board *);
+// a function for a 2 player game
+void startGame();
+// a function that provides a menu
+void menu();
+// this function decides if a move is correct or not for the bot
+int goodMove(struct board, int, int);
+// this function asks the user for a Row to take from
+int getRow();
+// a function for a game against the compute
+void playBot();
+
+void removeMatches(struct board* myBoard, int  index, int matches);
+
+int matchesLeft(struct board* myBoard);
 
 void startGame();
 
-void menu();
+int nimSum(struct board myBoard);
 
-int goodMove(struct board, int, int);
 
-void playBot();
 
 void generateBoard(struct board* myBoard){
     for (int i = 0; i < boardSize; i++){
