afc07540028ef30de5db419da9ca53a3a178739a
yo_boi <abdelrahmanbbb@gmail.com>
2023-03-24 03:25:45 +0300

    implemented the menu and showBoard functions
    Signed-off-by: yo_boi <abdelrahmanbbb@gmail.com>

--- 120200128_cmpe312_hw01.c
+++ 120200128_cmpe312_hw01.c
@@ -18,26 +18,24 @@ struct board{
 int player = 0;
 
 // this function prints a board nicely
-void showBoard(struct board *);
+void showBoard(struct board myBoard);
 // a function for a 2 player game
-void startGame();
+void startGame(struct board* myBoard);
 // a function that provides a menu
 void menu();
 // this function decides if a move is correct or not for the bot
-int goodMove(struct board, int, int);
+int goodMove(struct board, int index, int matches);
 // this function asks the user for a Row to take from
 int getRow();
 // a function for a game against the compute
-void playBot();
+void playBot(struct board *myBoard);
 // this function removes a number of matches from the chosen row
 void removeMatches(struct board* myBoard, int  index, int matches);
 // this function checks if any matches are left
 int matchesLeft(struct board* myBoard);
 // this function returns the nimSum of the game
 int nimSum(struct board myBoard);
-
-
-
+// this function fills the board with the appropriate number of matches
 void generateBoard(struct board* myBoard){
     for (int i = 0; i < boardSize; i++){
         // put the number of matchsticks depending on which row it is
@@ -47,5 +45,60 @@ void generateBoard(struct board* myBoard){
 
 int main(){
     puts("hello");
+    menu();
     return 0;
 };
+
+void menu(){
+    // this variable tracks if the game is still being played
+    int playing = 1;
+    while (playing){
+        struct board myBoard;
+        //populate the board
+        generateBoard(&myBoard);
+        int choice;
+        printf("[1] two players\n[2] play against computer\n[3] exit\nEnter a number: ");
+        scanf("%d", &choice);
+
+        switch(choice){
+            case 1:
+                // start a 2 player game
+                showBoard(myBoard);
+                startGame(&myBoard);
+                break;
+
+            case 2:
+                // start a game against the computer
+                showBoard(myBoard);
+                playBot(&myBoard);
+                break;
+
+            case 3:
+                //exit the game by setting the variable to end
+                playing = 0;
+                break;
+
+            default:
+                printf("\nchoose one of the options\n");
+                break;
+        }
+    }
+}
+
+void showBoard(struct board myBoard){
+    // print each row
+    for (int i = 0; i < boardSize; i++){
+        // print the row number
+        printf("%d" , i + 1);
+        //print the appropriate number of spaces
+        for (int j = maxIs - myBoard.arr[i]; j > 0; j--){
+            printf(" ");
+        }
+        // print the 'I's for the row
+        for (int j = 0; j < myBoard.arr[i]; j++){
+            printf("I ");
+        }
+        //put each row on a different line
+        printf("\n");
+    }
+}
